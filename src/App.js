a85dc619ad26cfef1a0a74c8be124977e3b05bc4
Tufail Khan <112545445+tufailKhan-dev@users.noreply.github.com>
2024-07-09 10:52:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Box from './components/Lab.jsx';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Box name="tufail"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Lab.jsx
@@ -0,0 +1,5 @@
+function Box({name}){
+    return <div>Hello.. I am {name}</div>;
+}
+
+export default Box
